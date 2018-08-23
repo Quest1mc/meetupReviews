9e171a58735f1182e884799b8fa41a3616749524
Hasan_sh <hasansh343@gmail.com>
2018-08-23 15:12:29 +0200

    home component

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,21 +1,71 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+// import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      meetups: []
+    }
+  }
+
+  onChange = e => {
+    console.log(e.target.value)
+    this.setState({ email: e.target.value })
+  }
+
+  componentDidMount = () => {
+    // this.service.get
+    // Todo: send the email in the body!!!!! not in the url!
+    fetch('/api/meetups')
+      .then(res => res.json())
+      .then(meetups => {
+        console.log(meetups)
+        let ms = []
+        meetups.forEach(meetup => {
+          // if (meetup.personEmail === this.state.email) {
+          const { name } = meetup
+          ms.push(<li key={meetup.name}>{name}</li>)
+          // }
+        })
+
+        this.setState({ meetups: ms })
+      })
+      .catch(err => console.error(err))
+  }
+
   render() {
+    const { meetups } = this.state
+    if (meetups.length > 0) {
+      console.log(meetups)
+    }
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Meetup extreme</h1>
+          {/* here should probable go the person's info!  
+              +
+            {person.name} instead of Hasan
+        */}
+          <h3>Hey Hasan!</h3>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <FindYourSelf/> */}
+        <div className="main">
+          <input
+            type="email"
+            onChange={this.onChange}
+            value={this.state.email}
+          />
+          <button onClick={this.getMeetups}>Search</button>
+
+          {/* this will be changed but it's good for now!!! */}
+          <ul>{meetups}</ul>
+        </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
